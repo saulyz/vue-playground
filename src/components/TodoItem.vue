f311ff64d8512898d09aee93618b2116fd627cb8
Saulius Vikerta <saulius.vikerta@nfq.lt>
2017-08-15 19:42:37 +0300

    Can delete tasks, use FontAwesome globally

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,17 +3,24 @@
     <small>{{number}}.</small>
     <input type="checkbox" :id="number" v-model="item.completed">
     <label :for="number" :class="{'completed': item.completed}">{{item.task}}</label>
+    - <span @click="remove"><icon name="trash"></icon></span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TodoItem',
-  props: ['item', 'index'],
+  props: ['item', 'index', 'callback'],
 
   data () {
     return {
       number: null
+    }
+  },
+
+  methods: {
+    remove () {
+      this.callback(this.index)
     }
   },
 
@@ -43,5 +50,9 @@ export default {
   .todo-item .completed {
     text-decoration: line-through;
     color: #999;
+  }
+
+  .todo-item .fa-icon {
+    color: #666;
   }
 </style>
--- src/components/TodoPage.vue
+++ src/components/TodoPage.vue
@@ -7,10 +7,11 @@
     <h3>Todo list tasks</h3>
       <ul>
         <li v-for="(todoItem, index) in todoList">
-          <todo-item :item="todoItem" :index="index"></todo-item>
+          <todo-item :item="todoItem" :index="index" :callback="removeTask"></todo-item>
         </li>
       </ul>
       <todo-add-item :callback="setNewTask"></todo-add-item>
+      <small class="note">Task list is stored in this page component, so if you navigate away, all tasks are lost</small>
   </div>
 </template>
 
@@ -31,8 +32,12 @@ export default {
   },
   methods: {
     setNewTask (text) {
-      this.todoList.push({task: text})
+      this.todoList.push({task: text, completed: false})
       console.log('new task was submitted:', text)
+    },
+    removeTask (index) {
+      console.log('remove item No:', index)
+      this.todoList.splice(index, 1)
     }
   }
 }
@@ -47,6 +52,15 @@ export default {
   li {
     margin: 0;
     padding: 0;
+  }
+  .fa-icon {
+    vertical-align: bottom;
+    margin-bottom: 4px;
+  }
+  .note{
+    display: block;
+    margin: 15px 0;
+    color: #999;
   }
 </style>
 
